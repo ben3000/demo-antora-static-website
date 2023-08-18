16b4bbe7eccd3ae951c50a0c023f189288c7e757
Ben Richardson <ben.richardson@dbca.wa.gov.au>
2023-08-18 14:12:32 +0800

    Adds a header to the generated CSV file.

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ print("Hello, CI/CD")
 with open('data.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',',
                         quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(["uuid", "name_id", "name1", "name2", "author"])
     writer.writerow([uuid.uuid4(), 1800, "Banksia", "attenuata", "R.Br."])
     writer.writerow([uuid.uuid4(), 21316, "Banksia", None, "L.f."])
     writer.writerow([uuid.uuid4(), 22793, "Proteaceae", None, "Juss."])

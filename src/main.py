48781c2bf88964b12897091a2b0e83cd5515f3d2
Ben Richardson <ben.richardson@dbca.wa.gov.au>
2023-08-18 14:03:33 +0800

    Adds some output to main.py to test the availability of Artifacts in GitHub.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
+import csv
+import uuid
+
 print("Hello, CI/CD")
+
+with open('data.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',',
+                        quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow([uuid.uuid4(), 1800, "Banksia", "attenuata", "R.Br."])
+    writer.writerow([uuid.uuid4(), 21316, "Banksia", None, "L.f."])
+    writer.writerow([uuid.uuid4(), 22793, "Proteaceae", None, "Juss."])
